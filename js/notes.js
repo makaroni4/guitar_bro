aa448af165b1115e399406083e546f9b5e7f90be
Anatoli Makarevich <makaroni4@gmail.com>
2017-06-03 16:37:46 +0200

    Add fret circles

--- js/notes.js
+++ js/notes.js
@@ -194,6 +194,15 @@ $(function() {
     rock.y -= canvas.height - currentY - rockHeight;
   });
 
+  function drawCircle(x, y) {
+    var circleSize = (blockHeight / 6 - 1) / 2;
+
+    ctx.fillStyle = "#FFF";
+    ctx.beginPath();
+    ctx.arc(x, y, circleSize, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
   function drawFretBoard() {
     ctx.fillStyle = "skyblue";
     ctx.fillRect(block.x, block.y, block.width, block.height);
@@ -204,6 +213,21 @@ $(function() {
       ctx.fillStyle = "#FFF";
       ctx.fillRect(i * rockWidth, block.y, pegWidth, block.height);
     }
+
+    // draw single circles
+    var circleFrets = [2, 4, 6, 8];
+    var cirlceColor = "#FFF";
+    var verticalMiddle = canvas.height - block.height / 2;
+    var circleSize = (blockHeight / 6 - 1) / 2;
+
+    circleFrets.forEach(function(fret) {
+      drawCircle((rockWidth * fret - 1) + rockWidth / 2 + pegWidth, verticalMiddle);
+    });
+
+    // draw double circles
+    var doubleCirclesFret = 12;
+    drawCircle((rockWidth * 11) + rockWidth / 2 + pegWidth, canvas.height - circleSize * 2.5);
+    drawCircle((rockWidth * 11) + rockWidth / 2 + pegWidth, canvas.height - blockHeight + 2.5 * circleSize);
   }
 
   function animate() {
